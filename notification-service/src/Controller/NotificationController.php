a098596381f3632ad3a26643335eeee6c6f19e5e
BelarbiLM <leomehdibelarbi@gmail.com>
2024-07-14 22:19:27 +0200

    Notification good

--- notification-service/src/Controller/NotificationController.php
+++ notification-service/src/Controller/NotificationController.php
@@ -4,39 +4,93 @@ namespace App\Controller;
 
 use App\Entity\Notification;
 use Doctrine\ORM\EntityManagerInterface;
-use PHPUnit\Framework\Error\Notice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class NotificationController extends AbstractController
 {
-    #[Route('/notification', name: 'app_notification')]
-    public function index(): Response
+    #[Route('/sendnotif', name: 'app_mail', methods: ['POST'])]
+    public function sendEmail(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('notification/index.html.twig', [
-            'controller_name' => 'NotificationController',
-        ]);
-    }
-    #[Route('/post', name: 'create_post', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
-    {
-        // Récupérer les données JSON de la requête
         $data = json_decode($request->getContent(), true);
-        // Créer une nouvelle instance de Post
+
+        if (!isset($data['sujet'], $data['recipient'], $data['message'])) {
+            return new JsonResponse(['error' => 'Not all expected data were provided.'], Response::HTTP_BAD_REQUEST);
+        }
+
         $notification = new Notification();
-        $notification->setEmailRecipient($data['email_recipient']);
-        $notification->setMessage($data['message']);
         $notification->setSujet($data['sujet']);
-        // Persister l'entité Post
+        $notification->setEmailRecipient($data['recipient']);
+        $notification->setMessage($data['message']);
+        
         $entityManager->persist($notification);
         $entityManager->flush();
-        // Retourner une réponse JSON
-        return new JsonResponse(
-            ['status' => 'Post created!'],
-            JsonResponse::HTTP_CREATED
-        );
+
+        $mail = new PHPMailer(true);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'annuelprojet2@gmail.com';
+            $mail->Password = 'zwcsygpubwzvaysr';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+            $mail->setFrom('annuelprojet2@gmail.com', 'Hello');
+            $mail->addAddress($data['recipient']); 
+            $mail->isHTML(true);
+
+            $mail->Subject = $data['sujet'];
+            $mail->Body    = $data['message'];
+            $mail->AltBody = strip_tags($data['message']);
+            
+            $mail->send();
+        } catch (Exception $e) {
+            return new JsonResponse(['error' => utf8_encode('PHP mailer error : ' . $mail->ErrorInfo)], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        return new JsonResponse(['success' => 'The email has been sent to the user '], Response::HTTP_OK);
+    }
+
+
+    #[Route('/notification/{id?}', name: 'get_notification', methods: ['GET'])]
+    public function getNotificationById(EntityManagerInterface $entityManager, $id = null): JsonResponse
+    {
+        $repository = $entityManager->getRepository(Notification::class);
+
+        if ($id !== null) {
+            $notification = $repository->find($id);
+
+            if (!$notification) {
+                return new JsonResponse(['error' => 'Notification not found'], Response::HTTP_NOT_FOUND);
+            }
+
+            return new JsonResponse([
+                'id' => $notification->getId(),
+                'sujet' => $notification->getSujet(),
+                'recipient' => $notification->getEmailRecipient(),
+                'message' => $notification->getMessage(),
+            ]);
+        } else {
+
+            $notifications = $repository->findAll();
+
+            $formattedNotifications = [];
+            foreach ($notifications as $notification) {
+                $formattedNotifications[] = [
+                    'id' => $notification->getId(),
+                    'sujet' => $notification->getSujet(),
+                    'recipient' => $notification->getEmailRecipient(),
+                    'message' => $notification->getMessage(),
+                ];
+            }
+
+            return new JsonResponse($formattedNotifications);
+        }
     }
 }
